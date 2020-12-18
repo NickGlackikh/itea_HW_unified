05c3383f1631ef262f7d747db50628f1d3b49002
Николай Глацких <nikolay.glatskikh@metinvestholding.com>
2020-12-18 20:10:20 +0200

    initial commit

---
+++ itea_lessons_unified/Lesson3Project/Controllers/NewsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using BasicInfo.Models;
+
+namespace Lesson3Project.Controllers
+{
+    public class NewsController:Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Content = NewsBase.News;
+            return View();
+        }
+    }
+}
---
+++ itea_lessons_unified/Lesson3Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lesson3Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
