7730960e3aec9894dc6e139c98f36b38903d6db7
Николай Глацких <nikolay.glatskikh@metinvestholding.com>
2021-02-16 19:48:22 +0200

    перезалил нужную версию ДЗ с сокетами

---
+++ WebSocketProject/Controllers/StreamController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebSocketProject.Services;
+
+namespace WebSocketProject.Controllers
+{
+    public class StreamController : Controller
+    {
+        private readonly IWebSocketHandler _handler;
+
+        public StreamController(IWebSocketHandler handler)
+        {
+            _handler = handler;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Chat()
+        {
+            return View();
+        }
+
+        public async Task Get(string username)
+        {
+            //Проверяем, является ли запрос сокет-запросом
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                //Если сокет-запрос - возвращаем клиенту ответ, что установили подключение 
+                WebSocket webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                await _handler.HandleAsync(username, webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = 400;
+            }
+        }
+
+        public async Task GetStream()
+        {
+            //Проверяем, является ли запрос сокет-запросом
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                //Если сокет-запрос - возвращаем клиенту ответ, что установили подключение 
+                WebSocket webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                await SendMessage(webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = 400;
+            }
+        }
+
+        private async Task SendMessage(WebSocket socket)
+        {
+            byte[] message = Encoding.UTF8.GetBytes("Hello World!");
+            //Бесконечный цикл для вывода сообщений
+            while(true)
+            {
+                await socket.SendAsync(new ArraySegment<byte>(message), WebSocketMessageType.Text,true, CancellationToken.None);
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+        }
+    }
+}
---
+++ itea_lessons_unified/Lesson4Project/Controllers/RestController.cs
@@ -0,0 +1,26 @@
+﻿using Lesson4Project.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson4Project.Controllers
+{
+    [Produces("application/json")]
+     public class RestController : Controller
+    {
+        private ICountryRepository countryrep { get; }
+        public RestController(IHumanRepository _humanrep, ICountryRepository _countryrep)
+        {
+            countryrep = _countryrep;
+        }
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return Json(countryrep.AllCountries().ToList());
+        }
+    }
+}
